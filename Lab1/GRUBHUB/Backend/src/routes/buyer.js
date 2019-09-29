004851cb3f73013e0cf6ffb5f6ca9c6b5b1e40c8
NithyaKuchadi <49386822+NithyaKuchadi@users.noreply.github.com>
2019-09-28 18:15:07 -0700

    Update buyer.js

--- Lab1/GRUBHUB/Backend/src/routes/buyer.js
+++ Lab1/GRUBHUB/Backend/src/routes/buyer.js
@@ -6,111 +6,90 @@ const restaurantDao = require("../dao/restaurantDao");
 const restaurantDaoobj = new restaurantDao();
 
 const sha1 = require('sha1');
-router.post("/getRestaurantNames",(req,res)=>
-{
-    let itemName = req.body.NameOfItem;
-    
-  try{
-    getRestaurantNames= async ()=>
-    {
-      let result=await menuDaoObj.getRestIDS(itemName);
-let restNames=[];
-          if(result[0])
-            {
-                for(let i=0;i<result.length;i++)
-                {
-                    let restid= result[i].idofrest;
-                  let restaurant= await restaurantDaoobj.getRestaurantsOnID(restid);
-                  console.log("restaurant id is "+restaurant);
-                  restNames.push(restaurant[0]);
-                  console.log("Image"+restaurant[0].RestaurantImage);
-                  console.log(restaurant[0].RestaurantName);
-                }
-        console.log("Restaurant Names:  "+restNames);
+router.post("/getRestaurantNames", (req, res) => {
+  let itemName = req.body.NameOfItem;
 
-              res.writeHead(200, {'content-type':'application/json'});
-              res.end(JSON.stringify(restNames));
-            }
-            else
-            {
-              res.status(200).json({responseMessage: 'Could not get all details'});
-            }
+  try {
+    getRestaurantNames = async () => {
+      let result = await menuDaoObj.getRestIDS(itemName);
+      let restNames = [];
+      if (result[0]) {
+        for (let i = 0; i < result.length; i++) {
+          let restid = result[i].idofrest;
+          let restaurant = await restaurantDaoobj.getRestaurantsOnID(restid);
+          
+          restNames.push(restaurant[0]);
+        }
+        
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify(restNames));
+      }
+      else {
+        res.status(200).json({ responseMessage: 'Could not get all details' });
+      }
     }
-    getRestaurantNames(); 
-}
-catch(error)
-{
-    res.status(500).json({responseMessage: 'Database not responding'});
-}
+    getRestaurantNames();
+  }
+  catch (error) {
+    res.status(500).json({ responseMessage: 'Database not responding' });
+  }
 });
-router.post("/getItemsSections",(req,res)=>
-{
-    let resID = req.body.RestaurantID;
-    console.log("did it come here?????"+resID);
-  try{
-    getItemsSections= async ()=>
-    {
-      let result=await menuDaoObj.getAllSections(resID);
-      console.log("result     "+result);
-      let itemdatalist=[];
-         if(result[0])
-            {
-                for(let i=0;i<result.length;i++)
-                {
-                    let sectionID= result[i].SectionID;
-                    let sectionName=result[i].SectionName;
-                    console.log("sectionID,  sectionName   "+sectionID+"------------"+sectionName);
-                   let items= await menuDaoObj.getAllItems(resID,sectionID);
-                   
-                  let itemdata={
-                    "SectionID":sectionID,
-                    "SectionName": sectionName,
-                    "Items": items
-                  }
-                  
-                  itemdatalist.push(itemdata);
-                }
-                console.log("itemList    "+itemdatalist);
-              res.writeHead(200, {'content-type':'application/json'});
-              res.end(JSON.stringify(itemdatalist));
-            }
-            else
-            {
-              res.status(200).json({responseMessage: 'Could not get Items'});
-            }
+router.post("/getItemsSections", (req, res) => {
+  let resID = req.body.RestaurantID;
+  
+  try {
+    getItemsSections = async () => {
+      let result = await menuDaoObj.getAllSections(resID);
+      
+      let itemdatalist = [];
+      if (result[0]) {
+        for (let i = 0; i < result.length; i++) {
+          let sectionID = result[i].SectionID;
+          let sectionName = result[i].SectionName;
+          
+          let items = await menuDaoObj.getAllItems(resID, sectionID);
+
+          let itemdata = {
+            "SectionID": sectionID,
+            "SectionName": sectionName,
+            "Items": items
+          }
+
+          itemdatalist.push(itemdata);
+        }
+        
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify(itemdatalist));
+      }
+      else {
+        res.status(200).json({ responseMessage: 'Could not get Items' });
+      }
     }
-    getItemsSections(); 
-}
-catch(error)
-{
-    res.status(500).json({responseMessage: 'Database not responding'});
-}
+    getItemsSections();
+  }
+  catch (error) {
+    res.status(500).json({ responseMessage: 'Database not responding' });
+  }
 });
 
-router.post("/getItemsONItemID",(req,res)=>
-{
-    let itemID = req.body.itemID;
-    console.log("Nithya here"+itemID);
-  try{
-    getItemsONItemID= async ()=>
-    {
-      let result=await menuDaoObj.getItemsBasedOnItemID(itemID);
-console.log("---------"+result[0].NameOfItem);
-          if(result[0])
-            {
-              res.writeHead(200, {'content-type':'application/json'});
-              res.end(JSON.stringify(result[0]));
-            }
-            else
-            {
-              res.status(200).json({responseMessage: 'Could not get all details'});
-            }
+router.post("/getItemsONItemID", (req, res) => {
+  let itemID = req.body.itemID;
+  
+  try {
+    getItemsONItemID = async () => {
+      let result = await menuDaoObj.getItemsBasedOnItemID(itemID);
+       if (result[0]) {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(JSON.stringify(result[0]));
+      }
+      else {
+        res.status(200).json({ responseMessage: 'Could not get all details' });
+      }
     }
-    getItemsONItemID(); 
-}
-catch(error)
-{
-    res.status(500).json({responseMessage: 'Database not responding'});
-}
+    getItemsONItemID();
+  }
+  catch (error) {
+    res.status(500).json({ responseMessage: 'Database not responding' });
+  }
 });
 module.exports = router;
