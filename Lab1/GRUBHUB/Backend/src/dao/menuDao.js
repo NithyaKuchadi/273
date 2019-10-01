d1d5508cef8b7a84684d2bab8ef16695934faf2e
NithyaKuchadi <49386822+NithyaKuchadi@users.noreply.github.com>
2019-10-01 16:44:13 -0700

    Update menuDao.js

--- Lab1/GRUBHUB/Backend/src/dao/menuDao.js
+++ Lab1/GRUBHUB/Backend/src/dao/menuDao.js
@@ -1,11 +1,13 @@
 const dBConnection = require('./ConnectionPooling');
 module.exports = class menuDao {
-    async addnewItem(inputData){
+    async addnewItem(inputData,RestaurantID,sectionID){
         let con = await dBConnection();
         try {
-            let savedUser = await con.query('INSERT INTO Items SET ?', [inputData]);
+             await con.query('INSERT INTO Items SET ?', [inputData]);
             await con.query("COMMIT");
-            return true;
+            let result=  await con.query('SELECT * FROM Items WHERE idofrest= ? AND sectID= ?' ,[RestaurantID,sectionID]);
+            await con.query("COMMIT");
+            return  JSON.parse(JSON.stringify(result));
           } catch (ex) {
             console.log(ex);
             await con.query("ROLLBACK");
@@ -35,6 +37,7 @@ module.exports = class menuDao {
         async deleteItem(itemid) {
             let con = await dBConnection();
             try {
+              console.log("ITem id in dao is"+itemid);
                 await con.query('DELETE FROM Items WHERE ItemID= ? ',[itemid]);
                 await con.query("COMMIT");
                 return true;
@@ -47,14 +50,14 @@ module.exports = class menuDao {
               }
             } 
             
-            async addnewSection(inputData){
+            async addnewSection(inputData,restID){
                 let con = await dBConnection();
                 try {
                     await con.query('INSERT INTO Sections SET ?', [inputData]);
                     await con.query("COMMIT");
-                    let sectionID=await con.query('SELECT SectionID FROM Sections WHERE ?', [inputData]);
+                    let result=await con.query('SELECT * FROM Sections WHERE idOfRestaurant= ?', [restID]);
                     await con.query("COMMIT");
-                    return sectionID;
+                    return result;
                   } catch (ex) {
                     console.log(ex);
                     await con.query("ROLLBACK");
@@ -66,12 +69,14 @@ module.exports = class menuDao {
                   }
             }
         
-            async updateSection(sectionName,sectionID) {
+            async updateSection(sectionName,sectionID,restid) {
                 let con = await dBConnection();
                 try {
                     await con.query('UPDATE Sections SET SectionName= ? WHERE SectionID= ?' ,[sectionName,sectionID]);
                     await con.query("COMMIT");
-                    return true;
+                    let result=await con.query('SELECT * FROM Sections WHERE idOfRestaurant= ?', [restid]);
+                    await con.query("COMMIT");
+                    return result;
                   } catch (ex) {
                     console.log(ex);
                     throw ex;
@@ -83,9 +88,10 @@ module.exports = class menuDao {
                 async getAllSections(RestaurantID) {
                   let con = await dBConnection();
                   try {
-                    let result=  await con.query('SELECT SectionID,SectionName FROM Sections WHERE idOfRestaurant= ?' ,[RestaurantID]);
+                    console.log("Restaurant id is "+RestaurantID);
+                    let result=  await con.query('SELECT * FROM Sections WHERE idOfRestaurant= ?' ,[RestaurantID]);
                       await con.query("COMMIT");
-                     
+                      console.log("in dao "+result[0]);
                      return  JSON.parse(JSON.stringify(result));
                     } catch (ex) {
                       console.log(ex);
@@ -114,8 +120,11 @@ module.exports = class menuDao {
                 try {
                     await con.query('DELETE FROM Sections WHERE idOfRestaurant = ? AND SectionID= ? ',[RestaurantID, sectionid]);
                     await con.query("COMMIT");
+                    let result=  await con.query('SELECT * FROM Sections WHERE idOfRestaurant= ?' ,[RestaurantID]);
+                    await con.query("COMMIT");
                     
-                    return true;
+                   return  JSON.parse(JSON.stringify(result));
+                    
                   } catch (ex) {
                     console.log(ex);
                     throw ex;
@@ -142,7 +151,7 @@ module.exports = class menuDao {
                   async getRestIDS(itemName) {
                     let con = await dBConnection();
                     try {
-                     
+                      console.log("Item name is "+itemName);
                       let result=  await con.query('SELECT * FROM Items WHERE NameOfItem= ?' ,[itemName]);
                         await con.query("COMMIT");
                        return  result;
@@ -154,7 +163,22 @@ module.exports = class menuDao {
                         await con.destroy();
                       }
                     }  
-    
+                    async getSectionID(RestaurantID,SectionName) {
+                      let con = await dBConnection();
+                      try {
+                        console.log("Restaurant id is "+RestaurantID);
+                        let result=  await con.query('SELECT * FROM Sections WHERE idOfRestaurant= ? AND SectionName = ?' ,[RestaurantID,SectionName]);
+                          await con.query("COMMIT");
+                          console.log("in dao "+result[0]);
+                         return  JSON.parse(JSON.stringify(result));
+                        } catch (ex) {
+                          console.log(ex);
+                          throw ex;
+                        } finally {
+                          await con.release();
+                          await con.destroy();
+                        }
+                      }   
     async getItemsBasedOnItemID(itemID) {
       let con = await dBConnection();
       try {
