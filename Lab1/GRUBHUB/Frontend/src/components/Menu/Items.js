84b3f674e6112b00322268a761d7fd9d7780223b
NithyaKuchadi <49386822+NithyaKuchadi@users.noreply.github.com>
2019-10-01 17:10:17 -0700

    Added Modal for Add Item

--- Lab1/GRUBHUB/Frontend/src/components/Menu/Items.js
+++ Lab1/GRUBHUB/Frontend/src/components/Menu/Items.js
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom';
 import cookie from 'react-cookies';
 import {Redirect} from 'react-router';
 import axios from 'axios';
+import {  Modal,Button } from 'react-bootstrap';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import '../Profile/ProfileOfBuyer.css';
 //create the Navbar Component
 class Items extends React.Component {
     constructor(props){
@@ -13,7 +15,14 @@ class Items extends React.Component {
             rowSelectItemID:"",
             completerowofitem:{},
             update:"false",
-            delete:"false"
+            delete:"false",
+            show:false,
+            itemName:"",
+            itemDescription:"",
+            itemPrice:0,
+            SectionName:"",
+            ItemImage:"",
+            ProfileImagePreview:undefined
         }
     }
 componentWillMount()
@@ -31,19 +40,44 @@ componentWillMount()
            });
        });
     } 
+    onAddItem=()=>
+    {
+        let addData={
+            "NameOfItem": this.state.itemName,
+            "DescriptionOfItem":this.state.itemDescription,
+            "PriceOfItem":this.state.itemPrice,
+            "UserID": cookie.load('cookie2'),
+            "SectionName":this.state.SectionName,
+            "ItemImage": this.state.ItemImage
+        }
+        axios.defaults.withCredentials = true;
+        axios.post('http://localhost:5000/addItem',addData)
+        .then((response) => {
+            console.log("In response..."+response.data);
+           this.setState(
+               {
+                showItems:response.data,
+               }
+           )
+
+       });
+
+    }
     onupdateOfItem=()=>
     { 
         let itemid=this.state.rowSelectItemID;
        let nameofitem= this.state.completerowofitem.NameOfItem;
        let descriptionofitem=this.state.completerowofitem.DescriptionOfItem;
        let priceofitem=this.state.completerowofitem.PriceOfItem;
+       let ItemImage=this.state.completerowofitem.ItemImage;
        let userid=cookie.load('cookie2');
         let updateddata={
             "ItemID":itemid,
             "NameOfItem": nameofitem,
             "DescriptionOfItem":descriptionofitem,
             "PriceOfItem":priceofitem,
-            "UserID":userid
+            "UserID":userid,
+            "ItemImage":ItemImage
         }
            axios.defaults.withCredentials = true;
         axios.post('http://localhost:5000/UpdateItem',updateddata)
@@ -56,6 +90,14 @@ componentWillMount()
            )
 
        });
+    }
+    onAdditionOfItem=()=>
+    {
+        this.setState(
+            {
+                show:true
+            }
+        )
     }
     ondeleteofItem=()=>
     {
@@ -81,6 +123,65 @@ componentWillMount()
           )
        });
     }
+    handleClose=()=>
+    {
+      this.setState(
+        {
+          show:false
+        }
+      )
+    }
+    handleItemName=(e)=>
+    {
+       this.setState(
+           {
+            itemName: e.target.value
+           }
+       )
+    }
+    handleItemDescription=(e)=>
+    {
+       this.setState(
+           {
+            itemDescription: e.target.value
+           }
+       )
+    }
+    handleItemPrice=(e)=>
+    {
+        this.setState(
+            {
+                itemPrice:e.target.value
+            }
+        )
+    }
+    handleItemImage=(e)=>
+    {
+        this.setState(
+            {
+                ItemImage: e.target.value
+            }
+        )
+        let imagefile=e.target.value;
+        imagefile=imagefile.slice(12);
+        axios.post('http://localhost:5000/download-file/' + imagefile)
+        .then(response => {
+            let imagePreview = 'data:image/jpg;base64, ' + response.data;
+            this.setState({
+                ProfileImagePreview: imagePreview
+            })
+
+        })
+
+    }
+    handleMenuSection=(e)=>
+    {
+        this.setState(
+            {
+                SectionName:e.target.value
+            }
+        )
+    }
     render() {
         const cellEditProp = {
             mode: 'click'
@@ -103,16 +204,50 @@ componentWillMount()
             onSelect: onRowSelect,
             clickToExpand: true  
           };
-
+          let profileImageData = <img className="img-style" src="https://img.freepik.com/free-icon/user-filled-person-shape_318-74922.jpg?size=338c&ext=jpg" alt="logo" />
+          if (this.state.ProfileImagePreview) {
+              profileImageData = <img className="img-style" src={this.state.ProfileImagePreview} alt="logo" />
+          }
         return (
             <div>
             <button style={ { backgroundColor: 'red' , color:"#fefefe"} } onClick={this.ondeleteofItem}>Delete Item</button>
-            <button style={ { backgroundColor: 'green' , color:"#fefefe"} } onClick={this.onupdateOfItem}>Update Item</button>
+            <button style={ { backgroundColor: 'orange' , color:"#fefefe"} } onClick={this.onupdateOfItem}>Update Item</button>
+            <button style={ { backgroundColor: 'green' , color:"#fefefe"} } onClick={this.onAdditionOfItem}>Add Item</button>
           <BootstrapTable data={this.state.showItems}  selectRow={selectRow} cellEdit={ cellEditProp}>
-             <TableHeaderColumn dataField='ItemID' isKey={ true } width="50">Item ID</TableHeaderColumn>
-             <TableHeaderColumn dataField='NameOfItem' width="50">Name of Item</TableHeaderColumn>
-              <TableHeaderColumn dataField='DescriptionOfItem' width="50">Description of item</TableHeaderColumn>
-              <TableHeaderColumn dataField='PriceOfItem' width="50">Price of Item</TableHeaderColumn>
+             <TableHeaderColumn dataField='ItemID' isKey={ true } hidden>Item ID</TableHeaderColumn>
+             <TableHeaderColumn dataField='NameOfItem' >Name of Item</TableHeaderColumn>
+             <TableHeaderColumn dataField='ItemImage'>Image</TableHeaderColumn>
+              <TableHeaderColumn dataField='DescriptionOfItem'>Description of item</TableHeaderColumn>
+              <TableHeaderColumn dataField='PriceOfItem'>Price of Item</TableHeaderColumn>
+              <div>
+              <Modal  className="setModal"  show={this.state.show} onHide={this.handleClose} centered={this.state.show}>
+             <Modal.Header  className="Itemsitem2">
+            <Modal.Title >Add Item</Modal.Title>
+             </Modal.Header>
+             <Modal.Body className="Itemsitem3">
+                     <div style={{ display: "inline-flex" }} >
+                    <div className="leftsidebutton143">
+                    {profileImageData}
+                    </div>
+                    <div className="rightsidebutton143">
+                    Image  <input type="file" name="ProfileImage" id="ProfileImage" className="btn btn-lg photo-upload-btn" onChange={this.handleItemImage} />  
+                     Name <input type="text" className="form-control" onChange={this.handleItemName}/>
+                     Description <input type="text" className="form-control" onChange={this.handleItemDescription}/>
+                     Price <input type="text" className="form-control" onChange={this.handleItemPrice}/>
+                     Menu Section <input type="text" className="form-control" onChange={this.handleMenuSection}/>
+                     </div>
+                     </div>
+              </Modal.Body>
+              <Modal.Footer  className="Itemsitem2">
+                    <Button variant="primary" className="s-btn s-btn-primary u-block-xs--down s-col-sm-5 s-col-xs-12">
+                    <span className="s-btn-copy" onClick={this.onAddItem}>Save</span>
+                    </Button>
+                     <Button variant="secondary" onClick={this.handleClose} className="s-btn s-btn-primary u-block-xs--down s-col-sm-5 s-col-xs-12">
+                     <span className="s-btn-copy">Close</span>
+                    </Button>
+                </Modal.Footer>
+              </Modal>
+                </div>
           </BootstrapTable>
           </div>);
       
