7ad407062b32989b333e229a169df1cb4314ff88
NithyaKuchadi <49386822+NithyaKuchadi@users.noreply.github.com>
2019-10-06 12:50:03 -0700

    removed background image

--- Lab1/GRUBHUB/Frontend/src/components/Home/Home.js
+++ Lab1/GRUBHUB/Frontend/src/components/Home/Home.js
@@ -6,42 +6,26 @@ import { Navbar } from "react-bootstrap";
 import '../Css/home.css';
 import Search from '../Buyer/Search';
 import { Link } from 'react-router-dom';
-import Background from "../Grubhub_homepageImage.webp";
+
 
 class Home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            backgroundImage: `url(${Background})`
-        }
-
+       
 
     }
 
     render() {
-        let searchView=null;
-        if(this.state.viewSearch)
-         {
-            let name=this.state.itemName;
-            console.log("it has come here"+this.state.itemName);
-        searchView= <Redirect to={{
-            pathname: '/Search',
-            state: 
-            { ItemName : name}
-        }}
-    />
-        
-        }
 
         return (
             <div>
-                <div >
-                    <div class="col-sm-8 home-banner-container">
-                        <h1>GRUBHUB</h1>
+                <div style={{ display: "inline-flex" }}>
+                    <div className="home-banner-container">
+                        <h2>GRUBHUB</h2>
                     </div>
-                    <div class="signinPage">
-                         <Link to="/Buyerlogin">Sign in</Link>
+                    <div className="rightsidebutton666">
+                       <p className="size"> <Link to="/Buyerlogin">Sign in</Link></p> 
                     </div>
                 </div>
             </div>
